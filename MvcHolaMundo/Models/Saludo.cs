17c6f163d49b9ce7018968f3769bc85618f650a6
luis@yo.es <luis@yo.es>
2015-11-10 14:24:06 +0100

    Fin

---
+++ MvcHolaMundo/Models/Saludo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcHolaMundo.Models
+{
+   public class Saludo
+    {
+       public String Idioma { get; set; }
+       public String Texto { get; set; }
+    }
+}
